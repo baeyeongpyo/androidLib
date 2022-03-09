54cd7653e3d04a0dcc3f883962c6859ef7b6722d
baeyeongpyo <byp93@naver.com>
2022-03-09 18:55:42 +0900

    dimensionUtil add.

--- core/src/main/java/com/yeong/core/component/resource/DimensionUtil.kt
+++ core/src/main/java/com/yeong/core/component/resource/DimensionUtil.kt
@@ -5,6 +5,10 @@ import android.content.Context
 private var dimensionSize = 0f
 
 object DimensionUtil {
+    /**
+     * [com.yeong.core.component.CoreModuleInitApp] 을 통해서 초기화를 하고 있지만,
+     * Custom View 를 사용하는 경우에서는 Init 해주지 못하므로 별도로 작업을 해주어야 preview 가 정상적으로 생성된다.
+     * */
     fun dimensionSizeInit(context: Context) {
         dimensionSize = context.resources.displayMetrics.density
     }
