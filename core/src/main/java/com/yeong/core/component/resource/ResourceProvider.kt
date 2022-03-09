be5c87f2ef7572630278c2c97b6753dac6edd937
baeyeongpyo <byp93@naver.com>
2022-03-09 18:40:53 +0900

    resource provider add.

---
+++ core/src/main/java/com/yeong/core/component/resource/ResourceProvider.kt
@@ -0,0 +1,34 @@
+package com.yeong.core.component.resource
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.FontRes
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+
+class ResourceProvider(private val context: Context) {
+
+    fun getString(@StringRes res: Int) = context.getString(res)
+
+    fun getString(@StringRes res: Int, vararg formatArgs: Any) = context.getString(res, *formatArgs)
+
+    fun getColor(@ColorRes res: Int) = ContextCompat.getColor(context, res)
+
+    fun getDrawable(@DrawableRes res: Int) = ContextCompat.getDrawable(context, res)
+
+    fun getAssertsManager() = context.assets
+
+    fun getFont(@FontRes font: Int) = ResourcesCompat.getFont(context, font)
+
+    fun permissionCheck(permission: String): Boolean =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
+        } else {
+            true
+        }
+
+}
