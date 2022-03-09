c76c41e8fe61ba423dd534fbce473238cdd2c09a
baeyeongpyo <byp93@naver.com>
2022-03-09 15:11:21 +0900

    single event data add.

---
+++ core/src/main/java/com/yeong/core/Data/SingleEvent.kt
@@ -0,0 +1,18 @@
+package com.yeong.core.Data
+
+open class SingleEvent<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    fun peekContent(): T = content
+
+}
