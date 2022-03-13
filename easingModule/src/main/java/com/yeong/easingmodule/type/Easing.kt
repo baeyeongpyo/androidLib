fc72c6e35da19b8d815d1c24a57e4fcb9fce13b7
baeyeongpyo <byp93@naver.com>
2022-03-14 00:50:15 +0900

    return type fix

--- easingModule/src/main/java/com/yeong/easingmodule/type/Easing.kt
+++ easingModule/src/main/java/com/yeong/easingmodule/type/Easing.kt
@@ -2,6 +2,6 @@ package com.yeong.easingmodule.type
 
 interface Easing {
 
-    fun calculator(n: Number): Float
+    fun calculator(n: Number): Number
 
 }
--- easingModule/src/main/java/com/yeong/easingmodule/type/type/EasingSine.kt
+++ easingModule/src/main/java/com/yeong/easingmodule/type/type/EasingSine.kt
@@ -2,4 +2,4 @@ package com.yeong.easingmodule.type.type
 
 import com.yeong.easingmodule.type.Easing
 
-interface Sine : Easing
+interface EasingSine : Easing
