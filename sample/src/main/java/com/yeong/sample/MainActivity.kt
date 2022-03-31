e37d6c5b8f6f3a79fe168c0d4a35bf984b95c57d
baeyeongpyo <byp93@naver.com>
2022-04-01 01:41:54 +0900

    endless listener sample add.

--- sample/src/main/java/com/yeong/sample/MainActivity.kt
+++ sample/src/main/java/com/yeong/sample/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.yeong.sample
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.yeong.core.component.BindingActivity
+import com.yeong.core.list.endless.EndlessOnLoadMore
+import com.yeong.core.list.endless.EndlessScrollListener
 import com.yeong.core.logger.mLog
 import com.yeong.sample.databinding.ActivityMainBinding
 
@@ -23,6 +26,9 @@ class MainActivity : BindingActivity<ActivityMainBinding>() {
         binding.sampleTestButton.setOnClickListener {
             dialogTest.show(supportFragmentManager, "")
         }
+
+        val items = mutableListOf<String>()
+
         binding.sampleRvView.adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             override fun onCreateViewHolder(
                 parent: ViewGroup,
@@ -31,13 +37,13 @@ class MainActivity : BindingActivity<ActivityMainBinding>() {
                 object : RecyclerView.ViewHolder(
                     LayoutInflater.from(parent.context)
                         .inflate(android.R.layout.simple_list_item_1, parent, false)
-                ){}
+                ) {}
 
             override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-                holder.itemView.findViewById<TextView>(android.R.id.text1).text = "$position"
+                holder.itemView.findViewById<TextView>(android.R.id.text1).text = items[position]
             }
 
-            override fun getItemCount(): Int = 100
+            override fun getItemCount(): Int = items.size
         }
         ItemTouchHelper(object : ItemTouchHelper.Callback() {
             override fun getMovementFlags(
@@ -54,7 +60,10 @@ class MainActivity : BindingActivity<ActivityMainBinding>() {
                 viewHolder: RecyclerView.ViewHolder,
                 target: RecyclerView.ViewHolder
             ): Boolean {
-                recyclerView.adapter?.notifyItemMoved(viewHolder.adapterPosition, target.adapterPosition)
+                recyclerView.adapter?.notifyItemMoved(
+                    viewHolder.adapterPosition,
+                    target.adapterPosition
+                )
                 return true
             }
 
@@ -62,6 +71,26 @@ class MainActivity : BindingActivity<ActivityMainBinding>() {
                 binding.sampleRvView.adapter?.notifyItemRemoved(viewHolder.adapterPosition)
             }
         }).attachToRecyclerView(binding.sampleRvView)
+
+        val loadMoreKey = object : EndlessOnLoadMore<Int>() {
+            override fun initKey(): Int = 0
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onLoadMore(key: Int?, totalItemCount: Int) {
+                nextKey(key?.run { this + 1 } ?: 0)
+                val k = key ?: 0
+                IntRange(k * 20, k * 20 + 20)
+                    .map { "item : $it" }
+                    .toList()
+                    .run(items::addAll)
+                binding.sampleRvView.adapter?.notifyDataSetChanged()
+            }
+        }
+
+        loadMoreKey.loadMore(0)
+
+        binding.sampleRvView.addOnScrollListener(EndlessScrollListener(loadMoreKey))
+
     }
 
     override fun initViewBinding() {
